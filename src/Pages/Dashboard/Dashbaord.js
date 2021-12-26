5fb1fd50b07381ff363d19023b3dc965210fd96d
moshiur01 <mr.moshiur314@gmail.com>
2021-12-26 20:59:12 +0600

    manage user order done

--- src/Pages/Dashboard/Dashbaord.js
+++ src/Pages/Dashboard/Dashbaord.js
@@ -1,7 +1,103 @@
 import React from "react";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import "./Dashboard.css";
+import AddReview from "./MyOrders/AddReview/AddReview";
+import MyOrders from "./MyOrders/MyOrders";
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  let { path, url } = useRouteMatch();
+  return (
+    <>
+      <Navbar
+        style={{ backgroundColor: "#243352" }}
+        expand={false}
+        className="p-lg-3 p-md-3 sticky-top"
+        variant="dark"
+      >
+        <Container fluid>
+          <Navbar.Brand as={HashLink} to="/home" className="fs-2">
+            ALGO DIGITAL
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                DashBoard
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={HashLink} to="/" className="">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={HashLink} to={`${url}`} className="">
+                  My Orders
+                </Nav.Link>
+
+                <Nav.Link as={HashLink} to={`${url}/addReview`} className="">
+                  Add Review
+                </Nav.Link>
+                {/* admin */}
+                <Nav.Link as={HashLink} to="/home" className="">
+                  Make Admin
+                </Nav.Link>
+                <Nav.Link as={HashLink} to="/home" className="">
+                  Manage Orders
+                </Nav.Link>
+                <Nav.Link as={HashLink} to="/home" className="">
+                  Manage Services
+                </Nav.Link>
+                <Nav.Link as={HashLink} to="/home" className="">
+                  Add Services
+                </Nav.Link>
+                <Nav.Link as={HashLink} to="/home" className="">
+                  Manage Reviews
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route exact path={path}>
+          <MyOrders></MyOrders>
+        </Route>
+        <Route exact path={`${path}/myOrders`}>
+          <MyOrders></MyOrders>
+        </Route>
+
+        <Route exact path={`${path}/makePayment`}>
+          {/* <MakePayment></MakePayment> */}
+        </Route>
+        <Route exact path={`${path}/addReview`}>
+          <AddReview></AddReview>
+        </Route>
+
+        {/* <AdminRoute exact path={`${path}/makeAdmin`}>
+            <MakeAdmin></MakeAdmin>
+          </AdminRoute>
+
+          <AdminRoute exact path={`${path}/manageOrders`}>
+            <ManageOrders></ManageOrders>
+          </AdminRoute>
+          <AdminRoute exact path={`${path}/manageCars`}>
+            <ManageCars></ManageCars>
+          </AdminRoute>
+          <AdminRoute exact path={`${path}/addCars`}>
+            <AddCars></AddCars>
+          </AdminRoute> */}
+      </Switch>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, Table } from "react-bootstrap";
+import useAuth from "../../hooks/useAuth";
+const MyOrders = () => {
+  const { user } = useAuth();
+  const [orders, setOrders] = useState([]);
+  const [myOrders, setMyOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/orders")
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
+
+  // filter orders by user email
+  useEffect(() => {
+    const found = orders.filter((order) => order?.email === user.email);
+    setMyOrders(found);
+  }, [orders, user.email]);
+
+  //   handle delete order by user
+
+  const handleDelete = (id) => {
+    const url = `http://localhost:5000/orders/${id}`;
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const confirm = window.confirm("DO you Want to Delete?");
+        if (confirm) {
+          if (data.deletedCount > 0) {
+            alert("order Deleted");
+            const remaining = myOrders.filter((order) => order._id !== id);
+            setMyOrders(remaining);
+          }
+        }
+      });
+  };
+
+  return (
+    <div>
+      <Container className="top-service-left-heading text-center">
+        <h1>My Orders</h1>
+      </Container>
+
+      <Container className="">
+        <Table responsive="sm" className="text-center">
+          <thead>
+            <tr>
+              <th>User Name</th>
+              <th>User Email</th>
+              <th>Service Name</th>
+              <th>phone Number</th>
+              <th>Action</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myOrders.map((order) => (
+              <tr className="align-items-center" key={order._id}>
+                <td>{order?.name}</td>
+                <td>{order?.email}</td>
+                <td>{order?.serviceName}</td>
+                <td>{order?.phoneNumber}</td>
+
+                <td>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleDelete(order._id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+                <td>
+                  {/* {order._id === orderStatus ? (
+                  <span className="text-success">Shipped</span>
+                ) : (
+                  <span className="text-info">Pending</span>
+                )} */}
+                  <span className="text-success">Shipped</span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
+
+export default MyOrders;
